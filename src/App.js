288e063d85b31e610ce3125eb8776ab59c010393
falak Chandni <falak.chandni@codingninjas.com>
2023-03-09 13:23:02 +0530

    multiple contexts

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   const [total, setTotal] = useState(0);
   const [item, setItem] = useState(0);
   return (
+    // providing multiple contexts
     <itemContext.Provider value={{ item, setItem }}>
       <totalContext.Provider value={{ total, setTotal }}>
         <div className="App">
