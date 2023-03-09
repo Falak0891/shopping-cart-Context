9e55396f10c1834cabe803a54c04fd8d3867752b
Falak0891 <70525535+Falak0891@users.noreply.github.com>
2023-03-09 13:20:16 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,13 @@ import React from "react";
 import styles from "../styles/Total.module.css";
 import { itemContext } from "../itemContext";
 import { useContext } from "react";
-import { totalContext } from "../totalContext";
 
 function Navbar() {
-  const { item } = useContext(itemContext);
-  const { total } = useContext(totalContext);
+  const value = useContext(itemContext);
   return (
     <div className={styles.container}>
-      <h1>Total : &#x20B9; {total}</h1>
-      <h1>Items: {item}</h1>
+      <h1>Total : &#x20B9; {value.total}</h1>
+      <h1>Items: {value.item}</h1>
     </div>
   );
 }
